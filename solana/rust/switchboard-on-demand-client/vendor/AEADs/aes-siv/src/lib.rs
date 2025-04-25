6e237d712dc8c29e8fee857de58600c52217371a
Copybara <infra@switchboard.xyz>
2025-04-25 19:09:50 +0300

    Project import generated by Copybara.
    GitOrigin-RevId: c63fce3b66c7abe138c992df8fe1bb05b0390f80

---
+++ solana/rust/switchboard-on-demand-client/vendor/AEADs/aes-siv/src/lib.rs
@@ -0,0 +1,253 @@
+//! [AES-SIV][1] ([RFC 5297][2]):
+//! [Authenticated Encryption with Associated Data (AEAD)][3] cipher which also
+//! provides [nonce reuse misuse resistance][4].
+//!
+//! # Usage
+//!
+//! Simple usage (allocating, no associated data):
+//!
+//! ```
+//! use aes_siv::{Aes128SivAead, Key, Nonce}; // Or `Aes256SivAead`
+//! use aes_siv::aead::{Aead, NewAead};
+//!
+//! let key = Key::from_slice(b"an example very very secret key.");
+//! let cipher = Aes128SivAead::new(key);
+//!
+//! let nonce = Nonce::from_slice(b"any unique nonce"); // 128-bits; unique per message
+//!
+//! let ciphertext = cipher.encrypt(nonce, b"plaintext message".as_ref())
+//!     .expect("encryption failure!"); // NOTE: handle this error to avoid panics!
+//!
+//! let plaintext = cipher.decrypt(nonce, ciphertext.as_ref())
+//!     .expect("decryption failure!"); // NOTE: handle this error to avoid panics!
+//!
+//! assert_eq!(&plaintext, b"plaintext message");
+//! ```
+//!
+//! ## In-place Usage (eliminates `alloc` requirement)
+//!
+//! This crate has an optional `alloc` feature which can be disabled in e.g.
+//! microcontroller environments that don't have a heap.
+//!
+//! The [`AeadInPlace::encrypt_in_place`] and [`AeadInPlace::decrypt_in_place`]
+//! methods accept any type that impls the [`aead::Buffer`] trait which
+//! contains the plaintext for encryption or ciphertext for decryption.
+//!
+//! Note that if you enable the `heapless` feature of this crate,
+//! you will receive an impl of [`aead::Buffer`] for `heapless::Vec`
+//! (re-exported from the [`aead`] crate as [`aead::heapless::Vec`]),
+//! which can then be passed as the `buffer` parameter to the in-place encrypt
+//! and decrypt methods:
+//!
+//! ```
+//! # #[cfg(feature = "heapless")]
+//! # {
+//! use aes_siv::{Aes128SivAead, Key, Nonce}; // Or `Aes256SivAead`
+//! use aes_siv::aead::{AeadInPlace, NewAead};
+//! use aes_siv::aead::heapless::Vec;
+//!
+//! let key = Key::from_slice(b"an example very very secret key.");
+//! let cipher = Aes128SivAead::new(key);
+//!
+//! let nonce = Nonce::from_slice(b"any unique nonce"); // 128-bits; unique per message
+//!
+//! let mut buffer: Vec<u8, 128> = Vec::new();
+//! buffer.extend_from_slice(b"plaintext message");
+//!
+//! // Encrypt `buffer` in-place, replacing the plaintext contents with ciphertext
+//! cipher.encrypt_in_place(nonce, b"", &mut buffer).expect("encryption failure!");
+//!
+//! // `buffer` now contains the message ciphertext
+//! assert_ne!(&buffer, b"plaintext message");
+//!
+//! // Decrypt `buffer` in-place, replacing its ciphertext context with the original plaintext
+//! cipher.decrypt_in_place(nonce, b"", &mut buffer).expect("decryption failure!");
+//! assert_eq!(&buffer, b"plaintext message");
+//! # }
+//! ```
+//!
+//! [1]: https://github.com/miscreant/meta/wiki/AES-SIV
+//! [2]: https://tools.ietf.org/html/rfc5297
+//! [3]: https://en.wikipedia.org/wiki/Authenticated_encryption
+//! [4]: https://github.com/miscreant/meta/wiki/Nonce-Reuse-Misuse-Resistance
+
+#![no_std]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg"
+)]
+#![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
+pub use aead;
+
+pub mod siv;
+
+use crate::siv::Siv;
+use aead::{
+    consts::{U0, U16, U32, U64},
+    generic_array::{ArrayLength, GenericArray},
+    AeadCore, AeadInPlace, Buffer, Error, NewAead,
+};
+use aes::{Aes128, Aes256};
+use cipher::{NewCipher, StreamCipher};
+use cmac::Cmac;
+use core::{marker::PhantomData, ops::Add};
+use crypto_mac::{Mac, NewMac};
+use ctr::Ctr128BE;
+
+#[cfg(feature = "pmac")]
+use pmac::Pmac;
+
+/// Size of an AES-SIV key given a particular cipher
+pub type KeySize<C> = <<C as NewCipher>::KeySize as Add>::Output;
+
+/// AES-SIV keys
+pub type Key<KeySize> = GenericArray<u8, KeySize>;
+
+/// AES-SIV nonces
+pub type Nonce = GenericArray<u8, U16>;
+
+/// AES-SIV tags (i.e. the Synthetic Initialization Vector value)
+pub type Tag = GenericArray<u8, U16>;
+
+/// The `SivAead` type wraps the more powerful `Siv` interface in a more
+/// commonly used Authenticated Encryption with Associated Data (AEAD) API,
+/// which accepts a key, nonce, and associated data when encrypting/decrypting.
+pub struct SivAead<C, M>
+where
+    C: NewCipher<NonceSize = U16> + StreamCipher,
+    M: Mac<OutputSize = U16>,
+    <C as NewCipher>::KeySize: Add,
+    KeySize<C>: ArrayLength<u8>,
+{
+    key: GenericArray<u8, KeySize<C>>,
+    mac: PhantomData<M>, // TODO(tarcieri): include `M` in `KeySize` calculation
+}
+
+/// SIV AEAD modes based on CMAC
+pub type CmacSivAead<BlockCipher> = SivAead<Ctr128BE<BlockCipher>, Cmac<BlockCipher>>;
+
+/// SIV AEAD modes based on PMAC
+#[cfg(feature = "pmac")]
+#[cfg_attr(docsrs, doc(cfg(feature = "pmac")))]
+pub type PmacSivAead<BlockCipher> = SivAead<Ctr128BE<BlockCipher>, Pmac<BlockCipher>>;
+
+/// AES-CMAC-SIV in AEAD mode with 256-bit key size (128-bit security)
+pub type Aes128SivAead = CmacSivAead<Aes128>;
+
+/// AES-CMAC-SIV in AEAD mode with 512-bit key size (256-bit security)
+pub type Aes256SivAead = CmacSivAead<Aes256>;
+
+/// AES-PMAC-SIV in AEAD mode with 256-bit key size (128-bit security)
+#[cfg(feature = "pmac")]
+#[cfg_attr(docsrs, doc(cfg(feature = "pmac")))]
+pub type Aes128PmacSivAead = PmacSivAead<Aes128>;
+
+/// AES-PMAC-SIV in AEAD mode with 512-bit key size (256-bit security)
+#[cfg(feature = "pmac")]
+#[cfg_attr(docsrs, doc(cfg(feature = "pmac")))]
+pub type Aes256PmacSivAead = PmacSivAead<Aes256>;
+
+impl<M> NewAead for SivAead<Ctr128BE<Aes128>, M>
+where
+    M: Mac<OutputSize = U16>,
+{
+    type KeySize = U32;
+
+    fn new(key: &GenericArray<u8, Self::KeySize>) -> Self {
+        Self {
+            key: *key,
+            mac: PhantomData,
+        }
+    }
+}
+
+impl<M> NewAead for SivAead<Ctr128BE<Aes256>, M>
+where
+    M: Mac<OutputSize = U16>,
+{
+    type KeySize = U64;
+
+    fn new(key: &GenericArray<u8, Self::KeySize>) -> Self {
+        Self {
+            key: *key,
+            mac: PhantomData,
+        }
+    }
+}
+
+impl<C, M> AeadCore for SivAead<C, M>
+where
+    C: NewCipher<NonceSize = U16> + StreamCipher,
+    M: Mac<OutputSize = U16> + NewMac,
+    <C as NewCipher>::KeySize: Add,
+    KeySize<C>: ArrayLength<u8>,
+{
+    // "If the nonce is random, it SHOULD be at least 128 bits in length"
+    // https://tools.ietf.org/html/rfc5297#section-3
+    // TODO(tarcieri): generic nonce sizes
+    type NonceSize = U16;
+    type TagSize = U16;
+    type CiphertextOverhead = U0;
+}
+
+impl<C, M> AeadInPlace for SivAead<C, M>
+where
+    C: NewCipher<NonceSize = U16> + StreamCipher,
+    M: Mac<OutputSize = U16> + NewMac,
+    <C as NewCipher>::KeySize: Add,
+    KeySize<C>: ArrayLength<u8>,
+{
+    fn encrypt_in_place(
+        &self,
+        nonce: &GenericArray<u8, Self::NonceSize>,
+        associated_data: &[u8],
+        buffer: &mut dyn Buffer,
+    ) -> Result<(), Error> {
+        // "SIV performs nonce-based authenticated encryption when a component of
+        // the associated data is a nonce.  For purposes of interoperability the
+        // final component -- i.e., the string immediately preceding the
+        // plaintext in the vector input to S2V -- is used for the nonce."
+        // https://tools.ietf.org/html/rfc5297#section-3
+        Siv::<C, M>::new(self.key.clone())
+            .encrypt_in_place(&[associated_data, nonce.as_slice()], buffer)
+    }
+
+    fn encrypt_in_place_detached(
+        &self,
+        nonce: &GenericArray<u8, Self::NonceSize>,
+        associated_data: &[u8],
+        buffer: &mut [u8],
+    ) -> Result<GenericArray<u8, Self::TagSize>, Error> {
+        Siv::<C, M>::new(self.key.clone())
+            .encrypt_in_place_detached(&[associated_data, nonce.as_slice()], buffer)
+    }
+
+    fn decrypt_in_place(
+        &self,
+        nonce: &GenericArray<u8, Self::NonceSize>,
+        associated_data: &[u8],
+        buffer: &mut dyn Buffer,
+    ) -> Result<(), Error> {
+        Siv::<C, M>::new(self.key.clone())
+            .decrypt_in_place(&[associated_data, nonce.as_slice()], buffer)
+    }
+
+    fn decrypt_in_place_detached(
+        &self,
+        nonce: &GenericArray<u8, Self::NonceSize>,
+        associated_data: &[u8],
+        buffer: &mut [u8],
+        tag: &GenericArray<u8, Self::TagSize>,
+    ) -> Result<(), Error> {
+        Siv::<C, M>::new(self.key.clone()).decrypt_in_place_detached(
+            &[associated_data, nonce.as_slice()],
+            buffer,
+            tag,
+        )
+    }
+}
---
+++ solana/rust/switchboard-on-demand-client/vendor/AEADs/mgm/src/lib.rs
@@ -0,0 +1,215 @@
+//! Generic implementation of [Multilinear Galous Mode][1] [AEAD] construction.
+//!
+//! # Example
+//! ```
+//! # #[cfg(feature = "alloc")]
+//! # {
+//! use mgm::Mgm;
+//! use kuznyechik::Kuznyechik;
+//! use mgm::aead::{Aead, NewAead, generic_array::GenericArray};
+//!
+//! let key = GenericArray::from_slice(b"very secret key very secret key ");
+//! let cipher = Mgm::<Kuznyechik>::new(key);
+//!
+//! // 127-bit nonce value, since API has to accept 128 bits, first nonce bit
+//! // MUST be equal to zero, otherwise encryption and decryption will fail
+//! let nonce = GenericArray::from_slice(b"unique nonce val");
+//!
+//! // NOTE: handle errors to avoid panics!
+//! let ciphertext = cipher.encrypt(nonce, b"plaintext message".as_ref())
+//!     .expect("encryption failure!");
+//! let plaintext = cipher.decrypt(nonce, ciphertext.as_ref())
+//!     .expect("decryption failure!");
+//!
+//! assert_eq!(&plaintext, b"plaintext message");
+//! # }
+//! ```
+//!
+//! [1]: https://eprint.iacr.org/2019/123.pdf
+//! [AEAD]: https://en.wikipedia.org/wiki/Authenticated_encryption
+#![no_std]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg"
+)]
+#![warn(missing_docs, rust_2018_idioms)]
+use aead::{consts::U0, generic_array::GenericArray, AeadCore, AeadInPlace, Error, Key, NewAead};
+use cfg_if::cfg_if;
+use cipher::{BlockCipher, BlockEncrypt, NewBlockCipher};
+
+pub use aead;
+
+mod encdec;
+mod gf;
+mod sealed;
+
+use sealed::Sealed;
+
+#[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
+cpufeatures::new!(mul_intrinsics, "sse2", "ssse3", "pclmulqdq");
+
+/// MGM nonces
+pub type Nonce<NonceSize> = GenericArray<u8, NonceSize>;
+
+/// MGM tags
+pub type Tag<TagSize> = GenericArray<u8, TagSize>;
+
+type Block<C> = GenericArray<u8, <C as BlockCipher>::BlockSize>;
+// cipher, nonce, aad, buffer
+type EncArgs<'a, C> = (&'a C, &'a Block<C>, &'a [u8], &'a mut [u8]);
+// cipher, nonce, aad, buf, expected_tag
+type DecArgs<'a, C> = (&'a C, &'a Block<C>, &'a [u8], &'a mut [u8], &'a Block<C>);
+
+/// Trait implemented for block cipher sizes usable with MGM.
+// ideally we would use type level set, i.e. `U8 | U16`
+pub trait MgmBlockSize: sealed::Sealed {}
+
+impl<T: Sealed> MgmBlockSize for T {}
+
+/// Multilinear Galous Mode cipher
+#[derive(Clone, Debug)]
+pub struct Mgm<C>
+where
+    C: BlockEncrypt,
+    C::BlockSize: MgmBlockSize,
+{
+    cipher: C,
+}
+
+impl<C> From<C> for Mgm<C>
+where
+    C: BlockEncrypt,
+    C::BlockSize: MgmBlockSize,
+{
+    fn from(cipher: C) -> Self {
+        Self { cipher }
+    }
+}
+
+impl<C> NewAead for Mgm<C>
+where
+    C: BlockEncrypt + NewBlockCipher,
+    C::BlockSize: MgmBlockSize,
+{
+    type KeySize = C::KeySize;
+
+    fn new(key: &Key<Self>) -> Self {
+        Self::from(C::new(key))
+    }
+}
+
+impl<C> AeadCore for Mgm<C>
+where
+    C: BlockEncrypt,
+    C::BlockSize: MgmBlockSize,
+{
+    type NonceSize = C::BlockSize;
+    type TagSize = C::BlockSize;
+    type CiphertextOverhead = U0;
+}
+
+impl<C> AeadInPlace for Mgm<C>
+where
+    C: BlockEncrypt,
+    C::BlockSize: MgmBlockSize,
+{
+    fn encrypt_in_place_detached(
+        &self,
+        nonce: &Nonce<Self::NonceSize>,
+        adata: &[u8],
+        buffer: &mut [u8],
+    ) -> Result<Tag<Self::TagSize>, Error> {
+        // first nonce bit must be equal to zero
+        if nonce[0] >> 7 != 0 {
+            return Err(Error);
+        }
+        mgm_encrypt((&self.cipher, nonce, adata, buffer))
+    }
+
+    fn decrypt_in_place_detached(
+        &self,
+        nonce: &Nonce<Self::NonceSize>,
+        adata: &[u8],
+        buffer: &mut [u8],
+        expected_tag: &Tag<Self::TagSize>,
+    ) -> Result<(), Error> {
+        // first nonce bit must be equal to zero
+        if nonce[0] >> 7 != 0 {
+            return Err(Error);
+        }
+        mgm_decrypt((&self.cipher, nonce, adata, buffer, expected_tag))
+    }
+}
+
+cfg_if! {
+    if #[cfg(all(
+        any(target_arch = "x86_64", target_arch = "x86"),
+        not(feature = "force-soft")
+    ))] {
+        #[target_feature(enable = "pclmulqdq")]
+        #[target_feature(enable = "ssse3")]
+        #[target_feature(enable = "sse2")]
+        unsafe fn wrapper<R: Sized>(f: impl FnOnce() -> R) -> R {
+            f()
+        }
+        fn mgm_encrypt<C>(args: EncArgs<'_, C>) -> Result<Block<C>, Error>
+        where
+            C: BlockEncrypt,
+            C::BlockSize: MgmBlockSize,
+        {
+            if mul_intrinsics::get() {
+                // SAFETY: we have checked that the required target features
+                // are available
+                unsafe {
+                    wrapper(|| {
+                        encdec::encrypt::<
+                            C,
+                            gf::gf64_pclmul::Element,
+                            gf::gf128_pclmul::Element,
+                        >(args)
+                    })
+                }
+            } else {
+                encdec::encrypt::<C, gf::gf64_soft64::Element, gf::gf128_soft64::Element>(args)
+            }
+        }
+
+        fn mgm_decrypt<C>(args: DecArgs<'_, C>) -> Result<(), Error>
+        where
+            C: BlockEncrypt,
+            C::BlockSize: MgmBlockSize,
+        {
+            if mul_intrinsics::get() {
+                // SAFETY: we have checked that the required target features
+                // are available
+                unsafe {
+                    wrapper(|| {
+                        encdec::decrypt::<
+                            C,
+                            gf::gf64_pclmul::Element,
+                            gf::gf128_pclmul::Element,
+                        >(args)
+                    })
+                }
+            } else {
+                encdec::decrypt::<C, gf::gf64_soft64::Element, gf::gf128_soft64::Element>(args)
+            }
+        }
+    } else {
+        fn mgm_encrypt<C>(args: EncArgs<'_, C>) -> Result<Block<C>, Error>
+        where
+            C: BlockEncrypt,
+            C::BlockSize: MgmBlockSize,
+        {
+            encdec::encrypt::<C, gf::gf64_soft64::Element, gf::gf128_soft64::Element>(args)
+        }
+
+        fn mgm_decrypt<C>(args: DecArgs<'_, C>) -> Result<(), Error>
+        where
+            C: BlockEncrypt,
+            C::BlockSize: MgmBlockSize,
+        {
+            encdec::decrypt::<C, gf::gf64_soft64::Element, gf::gf128_soft64::Element>(args)
+        }
+    }
+}
