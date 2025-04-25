b305ca91edeffdc312a37ab051cf9b2964b0cdc2
Copybara <infra@switchboard.xyz>
2025-04-26 00:52:30 +0300

    Project import generated by Copybara.
    GitOrigin-RevId: d154112a702f3e3d253f7ebcede1806d32304fed

--- solana/rust/switchboard-on-demand-client/vendor/AEADs/eax/tests/aes128eax.rs
+++
@@ -1,6 +0,0 @@
-//! Test vectors from Appendix G:
-//! https://web.cs.ucdavis.edu/~rogaway/papers/eax.pdf
-use aes::Aes128;
-use eax::Eax;
-
-aead::new_test!(aes128eax, "aes128eax", Eax<Aes128>);
--- solana/rust/switchboard-on-demand-client/vendor/AEADs/mgm/tests/rfc9058.rs
+++
@@ -1,4 +0,0 @@
-//! Test vectors from: https://datatracker.ietf.org/doc/html/rfc9058
-
-aead::new_test!(kuznyechik, "kuznyechik", mgm::Mgm<kuznyechik::Kuznyechik>);
-aead::new_test!(magma, "magma", mgm::Mgm<magma::Magma>);
